2f2647ea40124b67a67c3c175306057cb2d37d0a
Julio Mojica <juliocmojica@gmail.com>
2023-04-29 19:25:35 -0400

    parse query

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,28 @@
 import { type Request, type Response } from 'express'
 
+interface MultiRestaurantQuery {
+  zipcode: string;
+  rating?: number;
+  food_type?: string;
+}
+
 export const getMultipleRestaurants = async (req: Request, res: Response): Promise<void> => {
   try {
+    const zipcode = req.params.zipcode
+    const query: MultiRestaurantQuery = {
+      zipcode
+    }
+
+    if (Object.keys(req.query).length > 0) {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+
+      if (req.query.rating !== undefined) {
+        query.rating = Number(req.query.rating)
+      }
+      if (req.query.food_type !== undefined) {
+        query.food_type = String(req.query.food_type)
+      }
+    }
     res.send('Multiple Restaurants')
   } catch (err) {
     console.error(err)
