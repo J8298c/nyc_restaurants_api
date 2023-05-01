3331791bcf08e85b893f7025e8a53df8b28360b6
Julio Mojica <juliocmojica@gmail.com>
2023-05-01 04:48:47 -0400

    Restaurant Interface

--- src/interfaces/Restaurant.ts
+++ src/interfaces/Restaurant.ts
@@ -1,4 +1,22 @@
+interface Address {
+  building: string
+  coord: number[]
+  street: string
+  zipcode: string
+}
+
+interface Grade {
+  date: Date
+  grade: string
+  score: number
+}
+
 export interface Restaurant {
-  name: string;
-  address: any;
+  _id?: string
+  address: Address
+  borough: string
+  cuisine: string
+  grades: Grade[]
+  name: string
+  restaurant_id: string
 }
